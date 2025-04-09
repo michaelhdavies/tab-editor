2737480ede065449fb4da056aa4fe0697e5cae58
joelogical <jmsmith.coding@gmail.com>
2025-04-08 18:43:57 -0700

    guitar neck css

--- Guitar-Neck-V4/src/styles/GuitarNeck.tsx
+++ Guitar-Neck-V4/src/styles/GuitarNeck.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import NeckBox from '../components/NeckBox';
-import './GuitarNeck.css';
+import React from "react";
+import NeckBox from "../components/NeckBox";
+import "./GuitarNeck.css";
 
 const GuitarNeck: React.FC = () => {
   return (
     <div className="guitar-neck">
-      <div className='headstock'/>  
-      <div className="neck-body">
-        <div className="nut">
+      <div className="headstock">
+        <div className="tuning-pegs">
+          <div className="tuning-pegs-row top">
+            <div className="tuning-peg"></div>
+            <div className="tuning-peg"></div>
+            <div className="tuning-peg"></div>
+          </div>
+          <div className="tuning-pegs-row bottom">
+            <div className="tuning-peg"></div>
+            <div className="tuning-peg"></div>
+            <div className="tuning-peg"></div>
+          </div>
         </div>
+      </div>
+      <div className="neck-body">
+        <div className="nut"></div>
         <NeckBox />
       </div>
     </div>
